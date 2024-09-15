2fd8ef085de055cd94e4f3d29ffb0b30f584c02f
CasperK1 <casper.kapiainen@metropolia.fi>
2024-09-15 22:17:05 +0300

    Placeholder game classes for now

--- src/index.js
+++ src/index.js
@@ -1,48 +1,140 @@
-import { io} from "socket.io-client";
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
+const scoreEl = document.querySelector('#scoreEl')
 
-const messageInput = document.getElementById('message-input');
-const joinInput = document.getElementById('join-input');
-const joinButton = document.getElementById('join-button');
-const sendButton = document.getElementById('send-button');
-const chatMessages = document.getElementById('chat-messages');
+canvas.width = innerWidth
+canvas.height = innerHeight
 
+const x = canvas.width / 2
+const y = canvas.height / 2
 
-let userName = '';
+const player = new Player(x, y, 10, 'white')
+const projectiles = []
+const enemies = []
+const particles = []
 
-const socket = io('http://localhost:3000');
-socket.on('connect', () => {
-    console.log('Connected to server with id ' + socket.id);
-})
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
 
-function sendMessage() {
-    const message = messageInput.value.trim();
-    if (message) {
-        addMessage('user', message);
-        messageInput.value = '';
+    let x
+    let y
 
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
     }
+
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+
+    enemies.push(new Enemy(x, y, radius, color, velocity))
+  }, 1000)
 }
 
-function addMessage(sender, text) {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('message');
-    messageElement.classList.add(sender + '-message');
-    messageElement.textContent = userName + ': ' + text;
-    chatMessages.appendChild(messageElement);
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+let animationId
+let score = 0
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  player.draw()
+
+  for (let index = particles.length - 1; index >= 0; index--) {
+    const particle = particles[index]
+
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1)
+    } else {
+      particle.update()
+    }
+  }
+
+  for (let index = projectiles.length - 1; index >= 0; index--) {
+    const projectile = projectiles[index]
+
+    projectile.update()
+
+    // remove from edges of screen
+    if (
+      projectile.x - projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      projectiles.splice(index, 1)
+    }
+  }
+
+  for (let index = enemies.length - 1; index >= 0; index--) {
+    const enemy = enemies[index]
+
+    enemy.update()
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+    //end game
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId)
+    }
+
+    for (
+      let projectilesIndex = projectiles.length - 1;
+      projectilesIndex >= 0;
+      projectilesIndex--
+    ) {
+      const projectile = projectiles[projectilesIndex]
+
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+      // when projectiles touch enemy
+      if (dist - enemy.radius - projectile.radius < 1) {
+        // create explosions
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 6),
+                y: (Math.random() - 0.5) * (Math.random() * 6)
+              }
+            )
+          )
+        }
+        // this is where we shrink our enemy
+        if (enemy.radius - 10 > 5) {
+          score += 100
+          scoreEl.innerHTML = score
+          gsap.to(enemy, {
+            radius: enemy.radius - 10
+          })
+          projectiles.splice(projectilesIndex, 1)
+        } else {
+          // remove enemy if they are too small
+          score += 150
+          scoreEl.innerHTML = score
+
+          enemies.splice(index, 1)
+          projectiles.splice(projectilesIndex, 1)
+        }
+      }
+    }
+  }
 }
 
-
-joinButton.addEventListener('click', e => {
-    userName=  joinInput.value
-    joinInput.value = '';
-
-});
-
-sendButton.addEventListener('click', sendMessage);
-messageInput.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        sendMessage();
-    }
-});
+animate()
+spawnEnemies()
