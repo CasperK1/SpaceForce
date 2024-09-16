6d478822d6ff8be8e207a3204a18b2ff5f657f65
CasperK1 <casper.kapiainen@metropolia.fi>
2024-09-16 23:34:33 +0300

    update

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
