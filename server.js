67e0dddba3fb6710ab635d9fc409390cfdf22525
CasperK1 <156774036+CasperK1@users.noreply.github.com>
2024-09-18 23:25:19 +0300

    Movement lag compensation done

--- server.js
+++ server.js
@@ -1,54 +1,54 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require("socket.io");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   pingInterval: 3000,
-  pingTimeout: 5000
+  pingTimeout: 5000,
 });
 const port = 3000;
 const backEndPlayers = {};
-const speed = 10;
+const speed = 15;
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`a user connected ${socket.id}`);
   backEndPlayers[socket.id] = {
     x: 1000 * Math.random(),
     y: 1000 * Math.random(),
     color: `hsl(${360 * Math.random()}, 100%, 50%)`,
-    sequenceNumber: 0
+    sequenceNumber: 0,
   };
-  io.emit('update-players', backEndPlayers); // Sends player data to frontend
+  io.emit("update-players", backEndPlayers); // Sends player data to frontend
 
   // Disconnect
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`user disconnected ${socket.id}`);
     delete backEndPlayers[socket.id];
-    io.emit('update-players', backEndPlayers); // Removes player from frontend too
+    io.emit("update-players", backEndPlayers); // Removes player from frontend too
   });
 
   // Player movement
-  socket.on('player-movement', ({key, sequenceNumber}) => {
+  socket.on("player-movement", ({ key, sequenceNumber }) => {
     backEndPlayers[socket.id].sequenceNumber = sequenceNumber;
     switch (key) {
-      case 'up':
-        backEndPlayers[socket.id].y -= speed
+      case "up":
+        backEndPlayers[socket.id].y -= speed;
         break;
-      case 'down':
-        backEndPlayers[socket.id].y += speed
+      case "down":
+        backEndPlayers[socket.id].y += speed;
         break;
-      case 'left':
-        backEndPlayers[socket.id].x -= speed
+      case "left":
+        backEndPlayers[socket.id].x -= speed;
         break;
-      case 'right':
+      case "right":
         backEndPlayers[socket.id].x += speed;
         break;
     }
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => {
-  io.emit('update-players', backEndPlayers);
+  io.emit("update-players", backEndPlayers);
 }, 15);
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d', {antialias: true});
-const pixelRatio = window.devicePixelRatio || 1
-const scoreEl = document.querySelector('#scoreEl')
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d", { antialias: true });
+const pixelRatio = window.devicePixelRatio || 1;
+const scoreEl = document.querySelector("#scoreEl");
 const socket = io();
 
 canvas.width = window.innerWidth * pixelRatio;
 canvas.height = window.innerHeight * pixelRatio;
 ctx.imageSmoothingEnabled = true;
-ctx.imageSmoothingQuality = 'high';
+ctx.imageSmoothingQuality = "high";
 
-const frontEndPlayers = {}
-const speed = 10
+const frontEndPlayers = {};
+const speed = 15;
 
-socket.on('update-players', (playerDataBackend) => {
+socket.on("update-players", (playerDataBackend) => {
   for (const id in playerDataBackend) {
-    const playerData = playerDataBackend[id]
+    const playerData = playerDataBackend[id];
 
     // If player does not exist frontend, create a new player
     if (!frontEndPlayers[id]) {
@@ -22,119 +22,137 @@ socket.on('update-players', (playerDataBackend) => {
         color: playerData.color,
         x: playerData.x,
         y: playerData.y,
-        radius: 20
-      })
+        radius: 20,
+      });
     } else {
-      // Update player position from backend data
-      frontEndPlayers[id].x = playerData.x
-      frontEndPlayers[id].y = playerData.y
-      const lastBackendInputIndex = playerInputs.forEach((input) => {
-        return backEndPlayers.sequenceNumber === input.sequenceNumber
-      })
-      if (lastBackendInputIndex > -1) {
-        playerInputs.splice(0, lastBackendInputIndex + 1)
+      if (id === socket.id) {
+        // Update player position from backend data
+        frontEndPlayers[id].x = playerData.x;
+        frontEndPlayers[id].y = playerData.y;
+
+        const lastBackendInputIndex = playerInputs.findIndex((input) => {
+          return playerData.sequenceNumber === input.sequenceNumber;
+        });
+
+        if (lastBackendInputIndex > -1) {
+          playerInputs.splice(0, lastBackendInputIndex + 1);
+        }
+        playerInputs.forEach((input) => {
+          frontEndPlayers[id].x += input.dx;
+          frontEndPlayers[id].y += input.dy;
+        });
+      } else {
+        frontEndPlayers[id].x = playerData.x;
+        frontEndPlayers[id].y = playerData.y;
+        gsap.to(frontEndPlayers[id], {
+          x: playerData.x,
+          y: playerData.y,
+          duration: 0.015,
+          ease: "linear",
+        });
       }
     }
   }
   // If ID does not exist in backend, remove player from frontend
   for (const id in frontEndPlayers) {
     if (!playerDataBackend[id]) {
-      delete frontEndPlayers[id]
+      delete frontEndPlayers[id];
     }
   }
-})
+});
 
-let animationId
+let animationId;
 
 function animate() {
-  animationId = requestAnimationFrame(animate)
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  animationId = requestAnimationFrame(animate);
+  ctx.fillStyle = "rgba(0,0,0,0.14)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (const id in frontEndPlayers) {
-    frontEndPlayers[id].draw()
+    frontEndPlayers[id].draw();
   }
 }
 
-animate()
+animate();
 const keys = {
-  up: {pressed: false},
-  left: {pressed: false},
-  down: {pressed: false},
-  right: {pressed: false}
-}
+  up: { pressed: false },
+  left: { pressed: false },
+  down: { pressed: false },
+  right: { pressed: false },
+};
 
 // Sequence number for lag compensation:
 // allows the server to reconstruct order of player inputs if they arrive delayed or out of order due to network issues
-const playerInputs = []
-let sequenceNumber = 0
+const playerInputs = [];
+let sequenceNumber = 0;
 
+//setInterval: same tick rate as server
 setInterval(() => {
   if (keys.up.pressed) {
-    sequenceNumber++
-    playerInputs.push({sequenceNumber, dx: 0, dy: -speed})
-    frontEndPlayers[socket.id].y -= speed
-    socket.emit('player-movement', {key: 'up', sequenceNumber})
+    sequenceNumber++;
+    playerInputs.push({ sequenceNumber, dx: 0, dy: -speed });
+    frontEndPlayers[socket.id].y -= speed;
+    socket.emit("player-movement", { key: "up", sequenceNumber });
   }
   if (keys.left.pressed) {
-    sequenceNumber++
-    playerInputs.push({sequenceNumber, dx: -speed, dy: 0})
-    frontEndPlayers[socket.id].x -= speed
-    socket.emit('player-movement', {key: 'left', sequenceNumber})
+    sequenceNumber++;
+    playerInputs.push({ sequenceNumber, dx: -speed, dy: 0 });
+    frontEndPlayers[socket.id].x -= speed;
+    socket.emit("player-movement", { key: "left", sequenceNumber });
   }
   if (keys.down.pressed) {
-    sequenceNumber++
-    playerInputs.push({sequenceNumber, dx: 0, dy: speed})
-    frontEndPlayers[socket.id].y += speed
-    socket.emit('player-movement', {key: 'down', sequenceNumber})
+    sequenceNumber++;
+    playerInputs.push({ sequenceNumber, dx: 0, dy: speed });
+    frontEndPlayers[socket.id].y += speed;
+    socket.emit("player-movement", { key: "down", sequenceNumber });
   }
   if (keys.right.pressed) {
-    sequenceNumber++
-    playerInputs.push({sequenceNumber, dx: speed, dy: 0})
-    frontEndPlayers[socket.id].x += speed
-    socket.emit('player-movement', {key: 'right', sequenceNumber})
+    sequenceNumber++;
+    playerInputs.push({ sequenceNumber, dx: speed, dy: 0 });
+    frontEndPlayers[socket.id].x += speed;
+    socket.emit("player-movement", { key: "right", sequenceNumber });
   }
-}, 15)
+}, 15);
 
-window.addEventListener('keydown', (e) => {
-  if (!frontEndPlayers[socket.id]) return // error handling if player does not exist
+window.addEventListener("keydown", (e) => {
+  if (!frontEndPlayers[socket.id]) return; // error handling if player does not exist
   switch (e.key) {
-    case 'w':
-    case 'ArrowUp':
-      keys.up.pressed = true
-      break
-    case 'a':
-    case 'ArrowLeft':
-      keys.left.pressed = true
-      break
-    case 's':
-    case 'ArrowDown':
-      keys.down.pressed = true
-      break
-    case 'd':
-    case 'ArrowRight':
-      keys.right.pressed = true
-      break
+    case "w":
+    case "ArrowUp":
+      keys.up.pressed = true;
+      break;
+    case "a":
+    case "ArrowLeft":
+      keys.left.pressed = true;
+      break;
+    case "s":
+    case "ArrowDown":
+      keys.down.pressed = true;
+      break;
+    case "d":
+    case "ArrowRight":
+      keys.right.pressed = true;
+      break;
   }
-})
+});
 
-window.addEventListener('keyup', (e) => {
+window.addEventListener("keyup", (e) => {
   switch (e.key) {
-    case 'w':
-    case 'ArrowUp':
-      keys.up.pressed = false
-      break
-    case 'a':
-    case 'ArrowLeft':
-      keys.left.pressed = false
-      break
-    case 's':
-    case 'ArrowDown':
-      keys.down.pressed = false
-      break
-    case 'd':
-    case 'ArrowRight':
-      keys.right.pressed = false
-      break
+    case "w":
+    case "ArrowUp":
+      keys.up.pressed = false;
+      break;
+    case "a":
+    case "ArrowLeft":
+      keys.left.pressed = false;
+      break;
+    case "s":
+    case "ArrowDown":
+      keys.down.pressed = false;
+      break;
+    case "d":
+    case "ArrowRight":
+      keys.right.pressed = false;
+      break;
   }
 });
